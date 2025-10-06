600e28ddbaf51860e87c2417502140599f17530e
JiyadArsalAsari <ziyadarsal28@gmail.com>
2025-10-06 21:26:31 +0700

    Menambah fungsi untuk membandingkan dua sisi(equal)

--- triangle.go
+++ triangle.go
@@ -14,3 +14,8 @@ func segitigaValid_103012300164(a, b, c float64) bool { //Luthfi Iriawan Fadhila
 	return max < sum
 }
 
+func equal_103012300351(x, y float64) bool { // Jiyad Arsal Asari
+	syarat:= 0.01 * math.Max(x, y)
+	return math.Abs(x-y) <= syarat
+}
+
